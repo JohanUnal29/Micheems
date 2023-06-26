75bebd2035babd21464e19bd5725d2fed782fadf
Nicolás <johan.ardilah@gmail.com>
2023-06-25 20:58:40 -0500

    correciones

--- src/components/Adopt/ItemListContainer.jsx
+++ src/components/Adopt/ItemListContainer.jsx
@@ -5,64 +5,37 @@ import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "../firebase/config";
 import Navbarr2 from '../sub-components/navbar/Navbarr2';
 import "./Adopt.css";
-import { HashTabla, Animal } from "./hashTable.js";
-
 
 const ItemListContainer = () => {
-  const hashTabla = new HashTabla(100);
 
-  const [animales, setAnimales] = useState([]);
-  const [animales2, setAnimales2] = useState([]);
+    const [animales, setAnimales] = useState([]);
 
-  const [titulo, setTitulo] = useState("Productos");
+    const [titulo, setTitulo] = useState("Productos");
 
-  const categoria = useParams().categoria;
+    const categoria = useParams().categoria;
 
-  useEffect(() => {
-    const animalesRef = collection(db, "Mascotas");
-    const q = categoria ? query(animalesRef, where("categoria", "==", categoria)) : animalesRef;
-
-    getDocs(q)
-      .then((resp) => {
-        const animalesData = resp.docs.map((doc) => {
-          return { ...doc.data(), id: doc.id }
-        });
-        setAnimales(animalesData);     
-
-      })
-      .catch((error) => {
-        console.log("Error getting documents: ", error);
-      });
-
-  }, [categoria]);
-
-  useEffect(() => {
-    const tablaAnimales = animales.map(animal => ({
-      id: animal.id,
-      nombre: animal.nombre,
-      raza: animal.raza,
-      vacunas: animal.vacunas,
-      caracter: animal.caracter,
-      categoria: animal.categoria,
-      ciudad: animal.ciudad,
-      condicionesEspeciales: animal.condicionesEspeciales,
-      edad: animal.edad,
-      enfermedad: animal.enfermedad,
-      genero: animal.genero,
-      imagen: animal.imagen
-    }));
-  
-    setAnimales2(tablaAnimales);
-  }, [animales]);
-  
-  console.log("animales2", animales2);
-
+    useEffect(() => {
+      
+      const animalesRef = collection(db, "Mascotas");
+      const q = categoria ? query(animalesRef, where("categoria", "==", categoria)) : animalesRef;
+      
+      getDocs(q)
+        .then((resp) => {
+          setAnimales( resp.docs.map((doc) => {
+            return { ...doc.data(), id: doc.id}
+          }))
+        })
+        console.log(animales);
+      
+    }, [categoria])
+    
+    
   return (
     <div className="adopt">
-      <Navbarr2></Navbarr2>
-      <ItemList animales={animales2} titulo={titulo} />
+        <Navbarr2></Navbarr2>
+        <ItemList animales={animales} titulo={titulo} />
     </div>
   )
 }
 
-export default ItemListContainer;
+export default ItemListContainer
